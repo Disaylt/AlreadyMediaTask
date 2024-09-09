f95fb931e71ad3ab31341bc8f2d71d225355fe7f
Disaylt <disofban@gmail.com>
2024-09-09 20:16:03 +0300

    init: create asteroid events

---
+++ src/NasaAsteroid.Domain/Events/NewAsteroidMassEvent.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using NasaAsteroid.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Domain.Events
+{
+    public class NewAsteroidMassEvent : BaseAsteroidEvent
+    {
+        public Mass OldValue { get; }
+        public Mass NewValue { get; }
+
+        public NewAsteroidMassEvent(Mass oldValue, Mass newValue, int id) : base(id)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
