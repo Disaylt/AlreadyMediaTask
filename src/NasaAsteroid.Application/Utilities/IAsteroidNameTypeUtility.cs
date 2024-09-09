7bfa78d4f0c66275e849f94d7672fc0e15a09902
Disaylt <disofban@gmail.com>
2024-09-10 00:37:37 +0300

    init: create utility contracts

---
+++ src/NasaAsteroid.Application/Utilities/IAsteroidNameTypeUtility.cs
@@ -0,0 +1,14 @@
+﻿using NasaAsteroid.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Utilities
+{
+    public interface IAsteroidNameTypeUtility
+    {
+        NameType Convert(string value);
+    }
+}
