4cdcd96b8fddf4b4170b5b68bbb30ff42db024b3
Disaylt <disofban@gmail.com>
2024-09-09 20:38:12 +0300

    init: create command

---
+++ src/NasaAsteroid.Application/Commands/AddOrUpdateAsteroidsCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NasaAsteroid.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Commands
+{
+    public class AddOrUpdateAsteroidsCommand : IRequest<Unit>
+    {
+        public IEnumerable<AsteroidDto> Asteroids { get; set; } = Enumerable.Empty<AsteroidDto>();
+    }
+}
---
+++ src/NasaAsteroid.Application/Commands/AddOrUpdateAsteroidsCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Commands
+{
+    public class AddOrUpdateAsteroidsCommandHandler : IRequestHandler<AddOrUpdateAsteroidsCommand, Unit>
+    {
+        public Task<Unit> Handle(AddOrUpdateAsteroidsCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
