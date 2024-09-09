feaf22567887fcb8ae39ba53b37eaf6b427d6c05
Disaylt <disofban@gmail.com>
2024-09-09 19:59:42 +0300

    init: add coordinates

---
+++ src/NasaAsteroid.Domain/ValueObjects/Coordinates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Domain.ValueObjects
+{
+    public class Coordinates : BaseValueObject
+    {
+        public decimal? Longitude { get; private set; }
+        public decimal? Latitude { get; private set; }
+
+        public Coordinates(decimal? longitude, decimal? latitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return Longitude;
+            yield return Latitude;
+        }
+    }
+}
