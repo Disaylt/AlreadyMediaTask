406c5d0bb3befc9f9d62a2a1856d16e3b74a3bdf
Disaylt <disofban@gmail.com>
2024-09-09 21:03:04 +0300

    init: create root exeption

---
+++ src/NasaAsteroid.Application/ApplicationLayerRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application
+{
+    public static class ApplicationLayerRegistration
+    {
+        public static void AddApplicationLayer(this IServiceCollection collection)
+        {
+            collection.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+        }
+    }
+}
---
+++ src/NasaAsteroid.Domain/Seed/RootExeption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Domain.Seed
+{
+    public class RootExeption
+    {
+        public HttpStatusCode StatusCode { get; }
+        public IEnumerable<string> Messages { get; }
+
+        public RootExeption(HttpStatusCode statusCode, IEnumerable<string> messages)
+        {
+            StatusCode = statusCode;
+            Messages = messages;
+        }
+    }
+}
