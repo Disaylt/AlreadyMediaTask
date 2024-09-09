0c7678c5306b5d6d333212b118fcf0e59c338890
Disaylt <disofban@gmail.com>
2024-09-09 20:02:51 +0300

    init: create asteroid entity

--- src/NasaAsteroid.Domain/Entities/Asteroid.cs
+++ src/NasaAsteroid.Domain/Entities/Asteroid.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NasaAsteroid.Domain.Enums;
+using NasaAsteroid.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,39 @@ namespace NasaAsteroid.Domain.Entities
 {
     public class Asteroid : BaseEntity
     {
+        public string Name { get; private set; }
+        public Mass Mass { get; private set; }
+        public DateOnly Year { get; private set; }
+        public Coordinates Coordinates { get; private set; }
+        public FallStatus FallStaus { get; private set; }
+        public NameType NameType { get; private set; }
+        public string RecClass { get; private set; }
+
+        protected Asteroid()
+        {
+
+        }
+
+        public Asteroid(
+            int id,
+            string name,
+            Mass mass,
+            DateOnly year,
+            Coordinates coordinates,
+            FallStatus fall,
+            NameType nameType,
+            string recClass) : this()
+        {
+            Id = id;
+            Name = name;
+            Mass = mass;
+            Year = year;
+            Coordinates = coordinates;
+            NameType = nameType;
+            FallStaus = fall;
+            RecClass = recClass;
+        }
+
 
     }
 }
