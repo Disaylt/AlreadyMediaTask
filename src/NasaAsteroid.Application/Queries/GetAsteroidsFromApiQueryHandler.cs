e832b000cfcf7c41d1c73da8a23f6e82f0694647
Disaylt <disofban@gmail.com>
2024-09-09 23:47:34 +0300

    feat: implement query

--- src/NasaAsteroid.Application/Queries/GetAsteroidsFromApiQueryHandler.cs
+++ src/NasaAsteroid.Application/Queries/GetAsteroidsFromApiQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using NasaAsteroid.Application.HttpClients;
 using NasaAsteroid.Application.Models;
+using NasaAsteroid.Application.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,22 @@ namespace NasaAsteroid.Application.Queries
     public class GetAsteroidsFromApiQueryHandler : IRequestHandler<GetAsteroidsFromApiQuery, IEnumerable<AsteroidDto>>
     {
         private readonly INasaAsteroidHttpClient _httpClient;
+        private readonly IAsteroidMapper _asteroidMapper;
 
-        public GetAsteroidsFromApiQueryHandler(INasaAsteroidHttpClient httpClient)
+        public GetAsteroidsFromApiQueryHandler(INasaAsteroidHttpClient httpClient,
+            IAsteroidMapper asteroidMapper)
         {
             _httpClient = httpClient;
+            _asteroidMapper = asteroidMapper;
         }
 
         public async Task<IEnumerable<AsteroidDto>> Handle(GetAsteroidsFromApiQuery request, CancellationToken cancellationToken)
         {
             IEnumerable<AsteroidWebModel> webAsteroids = await _httpClient.GetRangeAsync();
-            throw new NotImplementedException();
+
+            return webAsteroids
+                .Select(_asteroidMapper.FromWeb)
+                .ToList();
         }
     }
 }
