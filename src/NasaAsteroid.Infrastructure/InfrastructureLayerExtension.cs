4a8e76c2e6472d718cb856d195e7a6345b245b50
Disaylt <disofban@gmail.com>
2024-09-09 23:08:26 +0300

    init: add registration extensions

---
+++ src/NasaAsteroid.Infrastructure/InfrastructureLayerExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NasaAsteroid.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Infrastructure
+{
+    internal static class InfrastructureLayerExtension
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection collection, IConfiguration configuration)
+        {
+            IConfigurationSection connectionSection = configuration.GetSection("ConnectionStrings");
+
+            string dbConnection = connectionSection.GetValue<string>("nasaAsteroids") ?? string.Empty;
+
+            collection.AddDatabase(dbConnection);
+        }
+    }
+}
