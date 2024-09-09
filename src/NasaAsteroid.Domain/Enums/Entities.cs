e3d37366c2836fe92b93620114ff838a9fd0d2bf
Disaylt <disofban@gmail.com>
2024-09-09 20:02:00 +0300

    init: create enums

---
+++ src/NasaAsteroid.Domain/Enums/Entities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Domain.Enums
+{
+    public enum NameType
+    {
+        Unknown,
+        Valid
+    }
+
+    public enum FallStatus
+    {
+        Unknown,
+        Fell,
+        Found
+    }
+}
