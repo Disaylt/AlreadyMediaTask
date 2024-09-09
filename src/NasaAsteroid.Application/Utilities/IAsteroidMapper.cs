090718d3cf83b9120c61002aa2732b010ea1e93d
Disaylt <disofban@gmail.com>
2024-09-09 23:45:29 +0300

    init: create contract

---
+++ src/NasaAsteroid.Application/Utilities/IAsteroidMapper.cs
@@ -0,0 +1,14 @@
+﻿using NasaAsteroid.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Utilities
+{
+    public interface IAsteroidMapper
+    {
+        AsteroidDto FromWeb(AsteroidWebModel asteroid);
+    }
+}
