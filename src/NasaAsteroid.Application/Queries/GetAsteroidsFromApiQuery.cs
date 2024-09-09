42263b5d43762615aab2762f6bdae57fdaa31693
Disaylt <disofban@gmail.com>
2024-09-09 23:42:49 +0300

    init: create query

---
+++ src/NasaAsteroid.Application/Queries/GetAsteroidsFromApiQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NasaAsteroid.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Queries
+{
+    public class GetAsteroidsFromApiQuery : IRequest<IEnumerable<AsteroidDto>>
+    {
+
+    }
+}
---
+++ src/NasaAsteroid.Application/Queries/GetAsteroidsFromApiQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using NasaAsteroid.Application.HttpClients;
+using NasaAsteroid.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Queries
+{
+    public class GetAsteroidsFromApiQueryHandler : IRequestHandler<GetAsteroidsFromApiQuery, IEnumerable<AsteroidDto>>
+    {
+        private readonly INasaAsteroidHttpClient _httpClient;
+
+        public GetAsteroidsFromApiQueryHandler(INasaAsteroidHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<AsteroidDto>> Handle(GetAsteroidsFromApiQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<AsteroidWebModel> webAsteroids = await _httpClient.GetRangeAsync();
+            throw new NotImplementedException();
+        }
+    }
+}
