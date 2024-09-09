a9c89bf044546ba0f44ccc343d18d62b63d65699
Disaylt <disofban@gmail.com>
2024-09-09 20:27:32 +0300

    init: create http client contract

--- src/NasaAsteroid.Application/HttpClients/INasaAsteroidHttpClient.cs
+++ src/NasaAsteroid.Application/HttpClients/INasaAsteroidHttpClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NasaAsteroid.Application.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace NasaAsteroid.Application.HttpClients
 {
     public interface INasaAsteroidHttpClient
     {
+        Task<IReadOnlyCollection<AsteroidWebModel>> GetRangeAsync();
     }
 }
