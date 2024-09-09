c77c0d8e386833e5e2c6b9ec592bc3997bbd41b8
Disaylt <disofban@gmail.com>
2024-09-09 23:14:13 +0300

    init: implement asteroid repository

---
+++ src/NasaAsteroid.Infrastructure/Repositories/AsteroidRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NasaAsteroid.Domain.Entities;
+using NasaAsteroid.Domain.Repositories;
+using NasaAsteroid.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Infrastructure.Repositories
+{
+    internal class AsteroidRepository : GenericRepository<Asteroid>, IAsteroidRepository
+    {
+        public AsteroidRepository(AsteroidDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Dictionary<int, Asteroid>> GetBucketAsync(IEnumerable<int> ids, CancellationToken cancellationToken)
+        {
+            return await _dbSet
+                .Where(x => ids.Contains(x.Id))
+                .ToDictionaryAsync(x => x.Id, x => x, cancellationToken);
+        }
+    }
+}
