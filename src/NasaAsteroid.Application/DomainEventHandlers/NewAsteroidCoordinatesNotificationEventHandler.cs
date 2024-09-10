79f0f9b91affea584331429f899cbd04c3927490
Disaylt <disofban@gmail.com>
2024-09-10 10:44:25 +0300

    init: create event handler

---
+++ src/NasaAsteroid.Application/DomainEventHandlers/NewAsteroidCoordinatesNotificationEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NasaAsteroid.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.DomainEventHandlers
+{
+    internal class NewAsteroidCoordinatesNotificationEventHandler : INotificationHandler<NewAsteroidCoordinatesEvent>
+    {
+        public ILogger<NewAsteroidCoordinatesNotificationEventHandler> _logger;
+
+        public NewAsteroidCoordinatesNotificationEventHandler(ILogger<NewAsteroidCoordinatesNotificationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+        public Task Handle(NewAsteroidCoordinatesEvent notification, CancellationToken cancellationToken)
+        {
+            string oldValueView = JsonSerializer.Serialize(notification.OldValue);
+            string newValueView = JsonSerializer.Serialize(notification.NewValue);
+
+            _logger.LogInformation($"Update cooridnates for asteroid id:{notification.Id}. Old value - {oldValueView}, new value - {newValueView}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
