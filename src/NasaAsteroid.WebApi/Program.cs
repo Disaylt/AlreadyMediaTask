92319911de21c6c38504f67c16c7e9efef374e46
Disaylt <disofban@gmail.com>
2024-09-10 13:05:48 +0300

    feat: delete base controller

--- src/NasaAsteroid.WebApi/Program.cs
+++ src/NasaAsteroid.WebApi/Program.cs
@@ -2,10 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +10,6 @@ var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
