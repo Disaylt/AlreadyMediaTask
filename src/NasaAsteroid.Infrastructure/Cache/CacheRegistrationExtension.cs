3f7498e8ea5c87465eb3f7a9986d43948d3389f1
Disaylt <disofban@gmail.com>
2024-09-10 14:06:05 +0300

    init: implement chache repository

---
+++ src/NasaAsteroid.Infrastructure/Cache/CacheRegistrationExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Infrastructure.Cache
+{
+    public static class CacheRegistrationExtension
+    {
+        public static void AddChache(this IServiceCollection collection, string connectionString)
+        {
+            collection.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = connectionString;
+            });
+        }
+    }
+}
