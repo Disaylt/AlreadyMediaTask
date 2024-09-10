b7f53f3f97477e9826b1f0c51d34e33c06578173
Disaylt <disofban@gmail.com>
2024-09-10 10:52:14 +0300

    init: create event handler

---
+++ src/NasaAsteroid.Application/DomainEventHandlers/NewAsteroidMassNotificationEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NasaAsteroid.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.DomainEventHandlers
+{
+    public class NewAsteroidMassNotificationEventHandler : INotificationHandler<NewAsteroidMassEvent>
+    {
+        public ILogger<NewAsteroidMassNotificationEventHandler> _logger;
+
+        public NewAsteroidMassNotificationEventHandler(ILogger<NewAsteroidMassNotificationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(NewAsteroidMassEvent notification, CancellationToken cancellationToken)
+        {
+            string oldValueView = JsonSerializer.Serialize(notification.OldValue);
+            string newValueView = JsonSerializer.Serialize(notification.NewValue);
+
+            _logger.LogInformation($"Update mass for asteroid id:{notification.Id}. Old value - {oldValueView}, new value - {newValueView}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
