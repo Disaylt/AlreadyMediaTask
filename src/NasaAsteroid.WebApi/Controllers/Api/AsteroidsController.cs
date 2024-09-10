1da3c6ca3aa6d88b9039fc02c3d6aef63d65e090
Disaylt <disofban@gmail.com>
2024-09-10 13:16:06 +0300

    init: create asteroid controller

---
+++ src/NasaAsteroid.WebApi/Controllers/Api/AsteroidsController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NasaAsteroid.Application.Models;
+using NasaAsteroid.Application.Queries;
+using NasaAsteroid.Application.Specifications;
+
+namespace NasaAsteroid.WebApi.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsteroidsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AsteroidsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] GetAsteroidsSpecification queries)
+        {
+            GetAsteroidsQuery request = new GetAsteroidsQuery { Specification = queries };
+            IEnumerable<AsteroidDto> asteroids = await _mediator.Send(request);
+
+            return Ok(asteroids);
+        }
+    }
+}
