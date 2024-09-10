573ad92e01d51ec62fa53ce6a3a554668c223c14
Disaylt <disofban@gmail.com>
2024-09-10 09:54:33 +0300

    init: create value object

---
+++ src/NasaAsteroid.Domain/ValueObjects/Year.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Domain.ValueObjects
+{
+    public class Year : BaseValueObject
+    {
+        public int Valie { get; }
+
+        public Year(int value)
+        {
+            if (value > DateTime.UtcNow.Year) 
+                throw new ArgumentException("Year discovered exceeds the current year");
+
+            Valie = value;
+        }
+
+        protected override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return Valie;
+        }
+    }
+}
