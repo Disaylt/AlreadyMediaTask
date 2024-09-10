f2f97c484e2809c8fe9d412259347b6b78f421e7
Disaylt <disofban@gmail.com>
2024-09-10 11:28:32 +0300

    init: add queries

---
+++ src/NasaAsteroid.Application/Queries/GetAsteroidsQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using NasaAsteroid.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Queries
+{
+    public class GetAsteroidsQuery : IRequest<IEnumerable<AsteroidDto>>
+    {
+        public int? FilterMinYear { get; set; }
+        public int? FilterMaxYear { get; set; }
+        public string? ClassType { get; set; }
+        public string? NamePart { get; set; }
+    }
+}
---
+++ src/NasaAsteroid.Domain/Enums/Queries.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Domain.Enums
+{
+    public enum AsteroidGroup
+    {
+        None = 0,
+        Year
+    }
+
+    public enum AsteroidSort
+    {
+        ByYear,
+        ByQuantity,
+        ByMass,
+    }
+}
