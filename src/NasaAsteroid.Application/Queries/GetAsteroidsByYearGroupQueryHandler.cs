6837c67e6efc715bb48ce845e2a1001523d19d62
Disaylt <disofban@gmail.com>
2024-09-10 13:03:20 +0300

    init: create query

---
+++ src/NasaAsteroid.Application/Queries/GetAsteroidsByYearGroupQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NasaAsteroid.Application.Models;
+using NasaAsteroid.Application.Specifications;
+using NasaAsteroid.Domain.Entities;
+using NasaAsteroid.Domain.Seed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaAsteroid.Application.Queries
+{
+    public class GetAsteroidsByYearGroupQueryHandler : IRequestHandler<GetAsteroidsByYearGroupQuery, IEnumerable<AsteroidYearGroupDto>>
+    {
+        private readonly ISpecificationExecutor<GetAsteroidsGroupYearSpecification, AsteroidYearGroupDto> _specificationExecutor;
+
+        public GetAsteroidsByYearGroupQueryHandler(ISpecificationExecutor<GetAsteroidsGroupYearSpecification, AsteroidYearGroupDto> specificationExecutor)
+        {
+            _specificationExecutor = specificationExecutor;
+        }
+
+        public async Task<IEnumerable<AsteroidYearGroupDto>> Handle(GetAsteroidsByYearGroupQuery request, CancellationToken cancellationToken)
+        {
+            return await _specificationExecutor.ExecuteAsync(request.Specification);
+        }
+    }
+}
